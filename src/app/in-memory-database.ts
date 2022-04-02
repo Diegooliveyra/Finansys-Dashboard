03861513f283597cee447e2723a0f6c902e73c65
diego.oliveira <diego.oliveira@maida.health>
2022-04-02 16:29:14 -0300

    adding category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,13 +1,14 @@
+import { Category } from './pages/categories/shared/category.model';
 import { InMemoryDbService } from "angular-in-memory-web-api";
 
 export class InMemoryDatabase implements InMemoryDbService {
   createDb() {
-    const categories = [
+    const categories: Category[] = [
       { id: 1, name: "Moradia", description: "Pagamentos de Conta da Casa" },
       { id: 2, name: "Saúde", description: "Plano de saúde e Remédios" },
       { id: 3, name: "Lazer", description: "Cinema, parque, praia etc" },
       { id: 4, name: "Salário", description: "Recebimento de Salário" },
-      { id: 5, name: "Freelas", description: "Trabalhos como Freelancer" },
+      { id: 5, name: "Freelas", description: "Trabalhos como Freelancer" }
     ];
 
     return {categories};
