e056fcae86f7155c41f839f37e6e7252c607dd6d
diego.oliveira <diego.oliveira@maida.health>
2022-04-03 21:00:53 -0300

    improving category list component

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,18 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Category } from "./../shared/category.model";
+import { CategoryService } from "./../shared/category.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-category-list',
-  templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.scss']
+  selector: "app-category-list",
+  templateUrl: "./category-list.component.html",
+  styleUrls: ["./category-list.component.scss"],
 })
 export class CategoryListComponent implements OnInit {
+  public categories: Category[] = [];
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit() {
+    this.categoryService.getAll().subscribe(
+      (categories) => (this.categories = categories),
+      (error) => alert(error)
+    );
   }
 
-  deleteCategorie()  {
-    alert('Excluido')
+  deleteCategory(id: number) {
+    const mustDelete = confirm("Deseja mesmo excluir este item ?");
+
+    if (mustDelete)
+      this.categoryService.delete(id).subscribe(
+        () => {
+          this.categories = this.categories.filter(
+            (element) => element.id !== id
+          );
+        },
+        () => alert("Erro ao tentar excluir")
+      );
   }
 }
